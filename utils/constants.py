4d38f80f0f389efe6d243029b5faf93d8ee5ab33
muskenum <peter.varga96@gmail.com>
2020-05-17 11:57:28 +0200

    Added message-specific values and functions

--- utils/constants.py
+++ utils/constants.py
@@ -3,7 +3,7 @@ from netsim.netinterface import *
 # Üzenet ID-k
 HANDSHAKE_MESSAGE_ID = 'H'
 COMMAND_MESSAGE_ID = 'C'
-FILE_TRANSFER_MESSAGE_ID = 'F'          # H: handshake, C: commad, F: file transfer     állítsátok át ha gondoljátok.
+FILE_TRANSFER_MESSAGE_ID = 'F'  # H: handshake, C: command, F: file transfer     állítsátok át ha gondoljátok.
 
 # Lehetséges üzenet ID-k
 ID_SPACE = [HANDSHAKE_MESSAGE_ID, COMMAND_MESSAGE_ID, FILE_TRANSFER_MESSAGE_ID]
@@ -11,9 +11,9 @@ ID_SPACE = [HANDSHAKE_MESSAGE_ID, COMMAND_MESSAGE_ID, FILE_TRANSFER_MESSAGE_ID]
 # Lehetséges kliens azonosítók
 CLIENT_SPACE = network_interface.addr_space
 
-#Milyen ID-khoz milyen típúsok tartoznak (Validációhoz)
+# Milyen ID-khoz milyen típúsok tartoznak (Validációhoz)
 TYPE_SPACE = {
-    'H': ['NEW', 'NAC', 'REJ', 'FIN', 'FAC'],   # A HandshakeMessage lehetséges TYPE mezői
+    'H': ['NEW', 'NAC', 'REJ', 'FIN', 'FAC'],  # A HandshakeMessage lehetséges TYPE mezői
     'C': [],
-    'F': []
+    'F': ['UNW', 'DNW', 'UNA', 'DNA', 'SND', 'DAT', 'FIN', 'FNA']  # Fájlátvitel lehetséges TYPE mezői
 }
--- utils/enums.py
+++ utils/enums.py
@@ -11,4 +11,11 @@ class CommandMessageTypes:
 
 
 class FileTransferMessageTypes:
-    type = '???'
+    NEW_UPL = 'UNW'
+    NEW_DNL = 'DNW'
+    UPL_NEW_ACK = 'UNA'
+    DNL_NEW_ACK = 'DNA'
+    SEND = 'SND'
+    DAT = 'DAT'
+    FIN = 'FIN'
+    ACK_FIN = 'FNA'
